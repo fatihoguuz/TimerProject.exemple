143d8a2a21ba329e64c7f6b70e76247c2d963b94
Fatih Oguz <oguzfatih39@gmail.com>
2023-08-26 15:43:48 +0300

    Uygulama Tamamlandi

--- TimerProject/ViewController.swift
+++ TimerProject/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+    
+    var timer = Timer()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       counter = 10
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        labelText.text = "Time: \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+    }
+    @objc func timerFunction() {
+        labelText.text = "Time: \(counter)"
+        counter -= 1
+        if counter < 0 {
+            timer.invalidate()
+            labelText.text = "Download Complete"
+        }
+    }
 }
 
